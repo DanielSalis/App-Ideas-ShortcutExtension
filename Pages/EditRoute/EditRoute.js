a5a8a18948c5e6da79051ef07ff4f127c02153d8
Daniel Salis <danielsalis98@gmail.com>
2020-12-15 22:57:19 -0300

    feature: adiçao da funcionalidade de exclusao de atalhos singulares

--- Pages/EditRoute/EditRoute.js
+++ Pages/EditRoute/EditRoute.js
@@ -9,8 +9,23 @@ const loadShortcuts = () => {
 
     if (CurrentShortcuts) {
         CurrentShortcuts.forEach(element => {
+            let editIcon = document.createElement('img');
+            editIcon.src = '../../assets/delete-alert.png';
+            editIcon.className = "delete-icon";
+            editIcon.onclick = function (element) {
+                debugger;
+                deleteShortcut(element.target);
+            };
+
+            let span = document.createElement('span');
+            span.innerText = "Atalho: " + element.key;
+            span.setAttribute('key',element.key);
+
             let li = document.createElement('li');
-            li.innerText ="Atalho: " + element.key;
+            li.className = "edit-li";
+            
+            li.appendChild(span);
+            li.appendChild(editIcon);
             ul.appendChild(li);
         });
     } else {
@@ -18,5 +33,19 @@ const loadShortcuts = () => {
         li.innerText ="Você ainda não criou atalhos T_T";
         ul.appendChild(li);
     }
+}
+
+const deleteShortcut = (element) => {
+    const span = element.previousSibling;
+    const li = element.parentElement;
+
+    const key = span.getAttribute('key');
+
+    let elementFound = CurrentShortcuts.find(element => element.key == key);
+    CurrentShortcuts.pop(elementFound);
+
+    localStorage.setItem('shortcuts', JSON.stringify(CurrentShortcuts));
+
+    li.remove();
 }
 
